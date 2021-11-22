bbd5fe6c7c34d7eb040e17a007e6ddc5dd11c12e
ngDuyAnh <In_Drop@outlook.com>
2021-11-21 23:28:26 -0600

    Future resource to turn house data from csv to json.

--- dataParse/futureResource/ResourceToTurnCSV_Obj_For_JSON.js
+++ dataParse/futureResource/ResourceToTurnCSV_Obj_For_JSON.js
@@ -7,8 +7,8 @@ https://www.npmjs.com/package/csv-to-js-parser
 */
 
 // Local variable dictionary
-const HOUSE_DATA_CSV_FILEPATH = "../data/house_data.csv";
-const HOUSE_DATA_JSON_FILEPATH = "../data/house_data.json";
+const HOUSE_DATA_CSV_FILEPATH = "../../data/house_data.csv";
+const HOUSE_DATA_JSON_FILEPATH = "../../data/house_data.json";
 
 // Get the CSV data
 let houseDataCSV = fs.readFileSync(HOUSE_DATA_CSV_FILEPATH).toString();
